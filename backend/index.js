4b1ff17b7cc29702034cd684d2b5f028e7c66712
HunterX-7 <fraquin7@gmail.com>
2023-08-04 18:17:52 -0500

    Add the mongoDB connect and the routes to start

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import thinkRoutes from './routes/thinkRoutes.js';
 
 dotenv.config();
 
@@ -8,12 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/think', thinkRoutes);
+
 app.get('/', async(req, res) => {
     res.send('Hello from ThinkArt');
 })
 
 const startServer = async () => {
-    app.listen(5050, () => console.log('Server has started on port http://localhost:5050'));
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(5050, () => console.log('Server has started on port http://localhost:5050'));
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 startServer();
